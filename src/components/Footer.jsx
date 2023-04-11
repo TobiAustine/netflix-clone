a824572c3d06f1b0124ef0ef7846bed200279cdf
ThatDivDoctor <autineosiene@gmail.com>
2023-04-11 13:23:49 +0100

    copyright

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <p className='text-center text-red-600 mt-4'>Developed by  <a href="https://github.com/TobiAustine">@ThatDivDoctor</a></p>
+    
+    </>
+  )
+}
+
+export default Footer
